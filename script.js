fecf63690c63051052db92869e99fcfa645b7d4a
ANISH KUMAR <70019437+Amish2417@users.noreply.github.com>
2024-03-31 00:30:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	if(num==2){
+		return 1;
+	}
+	if(num==1){
+		return 0;
+	}
+	let first=fibonacci(num-1);
+	let second=fibonacci(num-2);
+	return first+second;
 }
 
 module.exports = fibonacci;
