548e8000b84f827af5a009d249457912dfbc7ed7
ANISH KUMAR <70019437+Amish2417@users.noreply.github.com>
2024-05-03 00:58:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function fibonacci(num) {
 	let first = 1;
 	let second = 0;
 	for(let i = 2;i<=num;i++){
-		second = zeroth+first;
-		zeroth = first;
-		first = second;
+		second = zeroth%1000000007+first%1000000007;
+		zeroth = first%1000000007;
+		first = second%1000000007;
 	}
-	return second;
+	return second%1000000007;
 }
 
 module.exports = fibonacci;
